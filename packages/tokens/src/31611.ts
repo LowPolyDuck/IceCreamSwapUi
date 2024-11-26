3ade19420fe5cd6ce33dd22b2ecd7510ac6730b5
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-11-26 16:17:31 -0500

    morestuff

--- packages/tokens/src/31611.ts
+++ packages/tokens/src/31611.ts
@@ -5,4 +5,5 @@ export const dogechainTokens = {
   wdoge: WETH9[ChainId.DOGE],
   ice: ICE[ChainId.DOGE],
   usdt: new ERC20Token(ChainId.DOGE, '0xD2683b22287E63D22928CBe4514003a92507f474', 18, 'USDT', 'Tether USD'),
+  limpeth: new ERC20Token(ChainId.DOGE, '0xC2d002e83016Ce8e9BB58ff8A006128B7C6c058e', 18, 'LIMPETH', 'ETH PUMP, BTC DUMP'),
 }
