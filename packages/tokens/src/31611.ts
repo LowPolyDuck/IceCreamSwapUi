33ece029a0ad4f9e3b90a1120ac25b6132ede2b2
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-11-26 15:11:15 -0500

    fixingtokens

--- packages/tokens/src/31611.ts
+++ packages/tokens/src/31611.ts
@@ -1,5 +1,7 @@
-import { ChainId, WETH9 } from '@pancakeswap/sdk'
+import {ChainId, ERC20Token, WETH9} from '@pancakeswap/sdk'
+import {ICE} from "./common";
 
-export const bobTokens = {
-  weth: WETH9[ChainId.BOB],
+export const shimmerTestnetTokens = {
+  wsmr: WETH9[ChainId.SHIMMER_TEST],
+  usdt: new ERC20Token(ChainId.SHIMMER_TEST, '0xC2d002e83016Ce8e9BB58ff8A006128B7C6c058e', 18, 'LIMPETH', 'ETH DUMP, BTC PUMP'),
 }
