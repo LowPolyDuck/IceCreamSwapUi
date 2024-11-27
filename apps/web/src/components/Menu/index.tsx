9ef72c017e66afd7025fe2f68fb3c5f6b6fb3b05
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-11-27 08:36:57 -0500

    Update index.tsx

--- apps/web/src/components/Menu/index.tsx
+++ apps/web/src/components/Menu/index.tsx
@@ -1,50 +1,104 @@
 import { languageList, useTranslation } from '@pancakeswap/localization'
 import { footerLinks, Menu as UikitMenu, NextLinkFromReactRouter, useModal } from '@pancakeswap/uikit'
+import USCitizenConfirmModal from 'components/Modal/USCitizenConfirmModal'
 import { NetworkSwitcher } from 'components/NetworkSwitcher'
-import { useCallback, useMemo } from 'react'
-import { useRouter } from 'next/router'
-import { useActiveChainId } from 'hooks/useActiveChainId'
+import PhishingWarningBanner from 'components/PhishingWarningBanner'
+import { useCakePrice } from 'hooks/useCakePrice'
 import useTheme from 'hooks/useTheme'
+import { useRouter } from 'next/router'
+import { useCallback, useMemo } from 'react'
+import { useActiveChainId } from 'hooks/useActiveChainId'
+import { usePhishingBanner } from '@pancakeswap/utils/user'
+import { IdType } from 'hooks/useUserIsUsCitizenAcknowledgement'
+import { BIG_ZERO } from '@pancakeswap/utils/bigNumber'
 import GlobalSettings from './GlobalSettings'
+import { SettingsMode } from './GlobalSettings/types'
 import { useMenuItems } from './hooks/useMenuItems'
 import UserMenu from './UserMenu'
 import { getActiveMenuItem, getActiveSubMenuItem } from './utils'
+import {ICE} from "@pancakeswap/tokens";
 
 const LinkComponent = (linkProps) => {
   return <NextLinkFromReactRouter to={linkProps.href} {...linkProps} prefetch={false} />
 }
 
-const Menu = () => {
+const Menu = (props) => {
+  const { chainId } = useActiveChainId()
   const { isDark, setTheme } = useTheme()
+  const cakePrice = useCakePrice()
   const { currentLanguage, setLanguage, t } = useTranslation()
   const { pathname } = useRouter()
-  const { chainId } = useActiveChainId()
-  
-  const menuItems = useMenuItems()
-  
-  return (
-    <UikitMenu
-      linkComponent={LinkComponent}
-      rightSide={
-        <>
-          <GlobalSettings />
-          <NetworkSwitcher />
-          <UserMenu />
-        </>
+  const [onUSCitizenModalPresent] = useModal(
+    <USCitizenConfirmModal title={t('IceCreamSwap Perpetuals')} id={IdType.PERPETUALS} />,
+    false,
+    false,
+    'usCitizenConfirmModal',
+  )
+  const showPhishingWarningBanner = false
+
+  const menuItems = useMenuItems(onUSCitizenModalPresent)
+
+  const activeMenuItem = getActiveMenuItem({ menuConfig: menuItems, pathname })
+  const activeSubMenuItem = getActiveSubMenuItem({ menuItem: activeMenuItem, pathname })
+
+  const toggleTheme = useMemo(() => {
+    return () => setTheme(isDark ? 'light' : 'dark')
+  }, [setTheme, isDark])
+
+  const getFooterLinks = useMemo(() => {
+    return footerLinks(t)
+  }, [t])
+
+  const subLinks = useMemo(() => {
+    if (activeSubMenuItem?.items?.length > 0) {
+      return activeSubMenuItem.items
+    }
+    for (const menuItem of menuItems) {
+      const parentSubLinks = menuItem.items?.find((item) => item.items?.includes(activeSubMenuItem))
+      if (parentSubLinks) {
+        return parentSubLinks.items
       }
-      isDark={isDark}
-      toggleTheme={setTheme}
-      currentLang={currentLanguage.code}
-      langs={languageList}
-      setLang={setLanguage}
-      links={menuItems}
-      subLinks={subLinks}
-      footerLinks={getFooterLinks}
-      activeItem={activeMenuItem?.href}
-      activeSubItem={activeSubMenuItem?.href}
-      buyCakeLabel={t('Buy ICE')}
-      buyCakeLink={`/swap?chainId=${chainId}&outputCurrency=${ICE[chainId]}`}
-    />
+    }
+
+    return activeMenuItem?.hideSubNav || activeSubMenuItem?.hideSubNav ? [] : activeMenuItem?.items
+  }, [activeMenuItem?.hideSubNav, activeMenuItem?.items, activeSubMenuItem, menuItems])
+
+  const linkComponent = useCallback((linkProps) => {
+    return <NextLinkFromReactRouter to={linkProps.href} {...linkProps} prefetch={false} />
+  }, [])
+
+  const rightSide = useMemo(() => {
+    return (
+      <>
+        <GlobalSettings mode={SettingsMode.GLOBAL} />
+        <NetworkSwitcher />
+        <UserMenu />
+      </>
+    )
+  }, [])
+
+  return (
+    <>
+      <UikitMenu
+        linkComponent={linkComponent}
+        rightSide={rightSide}
+        banner={showPhishingWarningBanner && typeof window !== 'undefined' && <PhishingWarningBanner />}
+        isDark={isDark}
+        toggleTheme={toggleTheme}
+        currentLang={currentLanguage.code}
+        langs={languageList}
+        setLang={setLanguage}
+        cakePriceUsd={cakePrice.eq(BIG_ZERO) ? undefined : cakePrice}
+        links={menuItems}
+        subLinks={subLinks}
+        footerLinks={getFooterLinks}
+        activeItem={activeMenuItem?.href}
+        activeSubItem={activeSubMenuItem?.href}
+        buyCakeLabel={t('Buy ICE')}
+        buyCakeLink={`/swap?chainId=${chainId}&outputCurrency=${ICE[chainId]}`}
+        {...props}
+      />
+    </>
   )
 }
 
