9235d0d931eca7a98773a2e8e2f56ca70a16498f
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-12-01 13:52:42 -0500

    Update utils.ts

--- apps/web/src/components/Menu/utils.ts
+++ apps/web/src/components/Menu/utils.ts
@@ -1,18 +1,8 @@
 import orderBy from 'lodash/orderBy'
 import { ConfigMenuItemsType } from './config/config'
 
-export const getActiveMenuItem = ({
-  pathname,
-  menuConfig,
-}: {
-  pathname: string
-  menuConfig: ConfigMenuItemsType[]
-}) =>
-  menuConfig.find(
-    (menuItem) =>
-      menuItem?.href && // Ensure menuItem.href is valid
-      (pathname.startsWith(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
-  )
+export const getActiveMenuItem = ({ pathname, menuConfig }: { pathname: string; menuConfig: ConfigMenuItemsType[] }) =>
+  menuConfig.find((menuItem) => pathname.startsWith(menuItem.href) || getActiveSubMenuItem({ menuItem, pathname }))
 
 export const getActiveSubMenuItem = ({ pathname, menuItem }: { pathname: string; menuItem?: ConfigMenuItemsType }) => {
   const subItems = menuItem?.items?.reduce((acc, item) => {
