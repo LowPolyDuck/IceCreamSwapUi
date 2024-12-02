d0c63730f7e81bb33e7473cd313ad148fe2e5330
LowPolyDuck <97544154+LowPolyDuck@users.noreply.github.com>
2024-12-02 11:27:29 -0500

    Update index.tsx

--- apps/web/src/views/RemoveLiquidity/index.tsx
+++ apps/web/src/views/RemoveLiquidity/index.tsx
@@ -163,7 +163,7 @@ export default function RemoveLiquidity({ currencyA, currencyB, currencyIdA, cur
       { name: 'verifyingContract', type: 'address' },
     ]
     const domain = {
-      name: 'icecreamswap.com LP',
+      name: 'dumpyswap.xyz LP',
       version: '1',
       chainId,
       verifyingContract: pair.liquidityToken.address as `0x${string}`,
